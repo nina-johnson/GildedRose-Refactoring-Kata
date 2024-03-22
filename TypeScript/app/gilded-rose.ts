350ecee88e9d72cfa400acfc1e3d5f9a6f0cb286
Nina Johnson <ninajohnson@Ninas-MacBook-Air.local>
2024-03-22 10:35:17 -0500

    move updating individual items logic to helper

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -1,4 +1,10 @@
-import { Items, decreaseQuality, increaseQuality } from "./helpers";
+import {
+  Items,
+  updateBrie,
+  updateConjured,
+  updateDefault,
+  updatePass
+} from "./helpers";
 
 export class Item {
   name: string;
@@ -23,45 +29,19 @@ export class GildedRose {
     for (let item of this.items) {
       switch (item.name) {
         case Items.Brie:
-          if (item.sellIn <= 0) {
-            item = increaseQuality(item, 2);
-          } else {
-            item = increaseQuality(item, 1);
-          }
-          item.sellIn --;
+          updateBrie(item);
           break;
         case Items.Pass:
-          if (item.sellIn <= 0) {
-            item.quality = 0;
-          } else if (item.sellIn <= 5) {
-            item = increaseQuality(item, 3);
-          } else if (item.sellIn <= 10) {
-            item = increaseQuality(item, 2);
-          } else {
-            item = increaseQuality(item, 1);
-          }
-          item.sellIn --;
+          updatePass(item);
           break;
         case Items.Sulfuras:
           // do not change item as it is legendary
           break;
         case Items.Conjured:
-          // making the assumption that because "Conjured" items decrease in value twice as fast,
-          // the amount of quality decreased after passing the sell by also doubles
-          if (item.sellIn <=0) {
-            item = decreaseQuality(item, 4);
-          } else {
-            item = decreaseQuality(item, 2);
-          }
-          item.sellIn --;
+          updateConjured(item);
           break;
         default:
-          if (item.sellIn <= 0) {
-            item = decreaseQuality(item, 2);
-          } else {
-            item = decreaseQuality(item, 1);
-          }
-          item.sellIn --;
+          updateDefault(item);
           break;
       }
     }
--- TypeScript/app/helpers.ts
+++ TypeScript/app/helpers.ts
@@ -6,7 +6,7 @@ export enum Items {
   Sulfuras = 'Sulfuras, Hand of Ragnaros',
   Conjured = 'Conjured Mana Cake',
 }
-  
+
 export const increaseQuality = (item: Item, increaseBy: number): Item => {
   item.quality += increaseBy;
   if (item.quality >= 50) {
@@ -14,13 +14,59 @@ export const increaseQuality = (item: Item, increaseBy: number): Item => {
   }
   return item;
 }
-  
+
 export const decreaseQuality = (item: Item, decreaseBy: number): Item => {
   item.quality -= decreaseBy;
-  if (item.quality < 0){
+  if (item.quality < 0) {
     item.quality = 0;
   }
   return item;
 }
-  
+
+export const updateBrie = (item: Item): Item => {
+  if (item.sellIn <= 0) {
+    item = increaseQuality(item, 2);
+  } else {
+    item = increaseQuality(item, 1);
+  }
+  item.sellIn --;
+  return item;
+}
+
+export const updatePass = (item: Item): Item => {
+  if (item.sellIn <= 0) {
+    item.quality = 0;
+  } else if (item.sellIn <= 5) {
+    item = increaseQuality(item, 3);
+  } else if (item.sellIn <= 10) {
+    item = increaseQuality(item, 2);
+  } else {
+    item = increaseQuality(item, 1);
+  }
+  item.sellIn --;
+  return item;
+}
+
+export const updateConjured = (item: Item): Item => {
+  // making the assumption that because "Conjured" items decrease in value
+  // twice as fast, the amount of quality decreased after passing the sell
+  // by also doubles
+  if (item.sellIn <=0) {
+    item = decreaseQuality(item, 4);
+  } else {
+    item = decreaseQuality(item, 2);
+  }
+  item.sellIn --;
+  return item;
+}
+
+export const updateDefault = (item: Item): Item => {
+  if (item.sellIn <= 0) {
+    item = decreaseQuality(item, 2);
+  } else {
+    item = decreaseQuality(item, 1);
+  }
+  item.sellIn --;
+  return item;
+}
   
