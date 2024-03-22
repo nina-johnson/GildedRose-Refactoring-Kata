83413bec83ad6bd216b738e46ce3f271d1a7b435
Nina Johnson <ninajohnson@Ninas-MacBook-Air.local>
2024-03-22 00:57:08 -0500

    moving helper code to new file

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -1,3 +1,5 @@
+import { Items, decreaseQuality, increaseQuality } from "./helpers";
+
 export class Item {
   name: string;
   sellIn: number;
@@ -8,29 +10,6 @@ export class Item {
     this.sellIn = sellIn;
     this.quality = quality;
   }
-}
-
-enum Items {
-  Brie = 'Aged Brie',
-  Pass = 'Backstage passes to a TAFKAL80ETC concert',
-  Sulfuras = 'Sulfuras, Hand of Ragnaros',
-  Conjured = 'Conjured Mana Cake',
-}
-
-const increaseQuality = (item: Item, increaseBy: number): Item => {
-  item.quality += increaseBy;
-  if (item.quality >= 50) {
-    item.quality = 50;
-  }
-  return item;
-}
-
-const decreaseQuality = (item: Item, decreaseBy: number): Item => {
-  item.quality -= decreaseBy;
-  if (item.quality < 0){
-    item.quality = 0;
-  }
-  return item;
 }
 
 export class GildedRose {
